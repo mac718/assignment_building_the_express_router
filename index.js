e952832c12450047703946bb9b6af1065401e0eb
mac718 <mac718@gmail.com>
2018-09-30 18:47:03 -0400

    Set up path parsing and parameterized routing

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get('/', (req, res) => {
   res.end('Hiya');
 });
 
+app.get('/thing/:param1/:param2', (req, res) => {
+  res.end(req.params.hi);
+})
+
+app.get('/thing/:param1/other/:param2', (req, res) => {
+  res.end(req.params.hi);
+})
+
 app.listen(3000, 'localhost');
